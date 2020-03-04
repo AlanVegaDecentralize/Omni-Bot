ab66fcd1b715421ff70eb0681a1c9d5167022fd5
Alan Vega <alanvega002@gmail.com>
2020-03-04 19:50:10 +0000

    request-auth-token

--- omni.js
+++ omni.js
@@ -8,14 +8,20 @@ const omniKEY = process.env.OMNI_KEY;
 async function makeRequest() {
 
     const config = {
-        method: 'POST',
-        url: `${omniURL}/request-token`,
-        apiKey: omniKEY
+        method: 'GET',
+        url: `${omniURL}/request-auth-token`,
+        apiKey: `${omniKEY}`
     };
 
     const omni = await axios(config)
         .then((response) => console.log(response))
         .catch((error) => console.log(error))
+
+    // axios.post(`${omniURL}/request-auth-token`, {
+    //     'apiKey': `${omniKEY}`
+    // })
+    // .then((response) => console.log(response.data))
+    // .catch((error) => console.log(error))
 };
 
 makeRequest();

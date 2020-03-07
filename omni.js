148f34c5f2a83ed6b697bef454501e6522613415
Alan Vega <alanvega002@gmail.com>
2020-03-07 00:52:41 +0000

    Adventure and twitter bearer tokens

--- omni.js
+++ omni.js
@@ -1,27 +1,24 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const omniURL = process.env.OMNI_API;
+const omniURL = process.env.OMNI_URL;
 const omniKEY = process.env.OMNI_KEY;
 
 
-async function makeRequest() {
 
-    const config = {
-        method: 'POST',
-        url: `${omniURL}/request-auth-token`,
-        apiKey: `${omniKEY}`
-    };
 
-    const omni = await axios(config)
-        .then((response) => console.log(response))
-        .catch((error) => console.log(error))
-
-    // axios.post(`${omniURL}/request-auth-token`, {
-    //     'apiKey': `${omniKEY}`
-    // })
-    // .then((response) => console.log(response.data))
-    // .catch((error) => console.log(error))
+function register() {
+    axios.post(`${omniURL}/request-auth-token`, {
+        "apiKey": omniKEY
+    })
+        .then(function (response) {
+            // handle success
+            console.log(response.data)
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
 };
 
-makeRequest();
+register();
---
+++ twit.js
@@ -0,0 +1,41 @@
+const Twitter = require("twitter-lite");
+require('dotenv').config();
+
+const 
+
+// const client = new Twitter({
+//     subdomain: "api",
+//     version: "1.1",
+//     consumer_key: process.env.TWIT_API_KEY,
+//     consumer_secret: process.env.TWIT_API_SECRET_KEY,
+//     access_token_key: process.env.TWIT_TOKEN,
+//     access_token_secret: process.env.TWIT_TOKEN_SECRET
+// });
+
+// client
+//     .get("account/verify_credentials")
+//     .then(results => {
+//         console.log("results", results);
+//     })
+//     .catch(console.error);
+
+async function twitterBearer() {
+    const 
+}
+
+
+async function boom() {
+
+    const user = new Twitter({
+        consumer_key: process.env.TWIT_API_KEY,
+        consumer_secret: process.env.TWIT_API_KEY_SECRET
+    });
+
+    const response = await user.getBearerToken();
+    const app = new Twitter({
+        bearer_token: response.access_token
+    });
+    console.log(app) 
+};
+
+boom();
